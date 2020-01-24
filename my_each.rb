2180ad161134b8fd84ca843931696d6fc2063dd1
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-24 23:20:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  
+  while array.length > 0
+  
+    
+     yield(array[i])
+     
+     i+=1
+     
+     end
 end
+
+my_each([1, 2, 3, 4]) do |number|
+  
+         puts "number"
+         end
